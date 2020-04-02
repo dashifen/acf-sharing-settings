d88fe22382a24ea3982f130fb6b9a0069cdf0c3a
David Dashifen Kees <dashifen@dashifen.com>
2020-04-02 10:27:53 -0400

    finding the right autoloader

--- acf-sharing-settings.php
+++ acf-sharing-settings.php
@@ -1,20 +1,30 @@
 <?php
+
 /**
  * Plugin Name: ACF Sharing Settings
  * Description: An ACF field group for a settings page relating to how a site appears on Twitter and Facebook.
  * Author URI: mailto:dashifen@dashifen.com
  * Author: David Dashifen Kees
- *Version: 1.2.0
+ * Version: 1.2.0
+ *
+ * @noinspection PhpStatementHasEmptyBodyInspection
+ * @noinspection PhpIncludeInspection
  */
 
 use Dashifen\WPHandler\Handlers\HandlerException;
 use Dashifen\ACFSharingSettings\ACFSharingSettings;
 
-if (file_exists($autoloader = realpath('./vendor/autoloader.php'))) {
-    /** @noinspection PhpIncludeInspection */
-    
-    require_once $autoloader;
-}
+// the following snippet finds the appropriate autoloader starting from a
+// where Dash likes to put it and ending at a reasonable (but unlikely)
+// default.  note that the $autoloader variable is set in the if-conditionals,
+// so they don't need statement bodies; all their work is done during each
+// test.  then, we try requiring what we end up with.
+
+if (file_exists($autoloader = dirname(ABSPATH) . '/deps/vendor/autoload.php'));
+elseif ($autoloader = file_exists(dirname(ABSPATH) . '/vendor/autoload.php'));
+elseif ($autoloader = file_exists(ABSPATH . 'vendor/autoload.php'));
+else $autoloader = 'vendor/autoload.php';
+require_once $autoloader;
 
 try {
     (new ACFSharingSettings())->initialize();
