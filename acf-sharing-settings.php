a95508f2a27df40f83b5154619382c4b8edf91ef
David Dashifen Kees <dashifen@dashifen.com>
2020-04-06 14:56:03 -0400

    bumping WP plugin version

--- acf-sharing-settings.php
+++ acf-sharing-settings.php
@@ -5,7 +5,7 @@
  * Description: An ACF field group for a settings page relating to how a site appears on Twitter and Facebook.
  * Author URI: mailto:dashifen@dashifen.com
  * Author: David Dashifen Kees
- * Version: 1.2.0
+ * Version: 1.3.1
  *
  * @noinspection PhpStatementHasEmptyBodyInspection
  * @noinspection PhpIncludeInspection
