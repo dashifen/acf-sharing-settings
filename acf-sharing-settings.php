c67e28b9212825cdd4c073c6bc041a721962d8a0
David Dashifen Kees <dashifen@dashifen.com>
2020-04-06 14:43:05 -0400

    wp-handler update; better autoloader inclusion

--- acf-sharing-settings.php
+++ acf-sharing-settings.php
@@ -14,20 +14,12 @@
 use Dashifen\WPHandler\Handlers\HandlerException;
 use Dashifen\ACFSharingSettings\ACFSharingSettings;
 
-// the following snippet finds the appropriate autoloader starting from a
-// where Dash likes to put it and ending at a reasonable (but unlikely)
-// default.  note that the $autoloader variable is set in the if-conditionals,
-// so they don't need statement bodies; all their work is done during each
-// test.  then, we try requiring what we end up with.
-
-if (file_exists($autoloader = dirname(ABSPATH) . '/deps/vendor/autoload.php'));
-elseif ($autoloader = file_exists(dirname(ABSPATH) . '/vendor/autoload.php'));
-elseif ($autoloader = file_exists(ABSPATH . 'vendor/autoload.php'));
-else $autoloader = 'vendor/autoload.php';
-require_once $autoloader;
-
-try {
-    (new ACFSharingSettings())->initialize();
-} catch (HandlerException $exception) {
-    wp_die('Unable to initialize ACF sharing settings.');
-}
+(function () {
+    try {
+        ACFSharingSettings::requireAutoloader();
+        $acfSharingSettings = new ACFSharingSettings();
+        $acfSharingSettings->initialize();
+    } catch (HandlerException $e) {
+        wp_die($e->getMessage());
+    }
+})();
