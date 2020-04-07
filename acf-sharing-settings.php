af988a6af53bda4cad43427f30691596073e74a9
David Dashifen Kees <dashifen@dashifen.com>
2020-04-07 12:45:26 -0400

    fixed autoloader

--- acf-sharing-settings.php
+++ acf-sharing-settings.php
@@ -5,7 +5,7 @@
  * Description: An ACF field group for a settings page relating to how a site appears on Twitter and Facebook.
  * Author URI: mailto:dashifen@dashifen.com
  * Author: David Dashifen Kees
- * Version: 1.3.1
+ * Version: 1.4.0
  *
  * @noinspection PhpStatementHasEmptyBodyInspection
  * @noinspection PhpIncludeInspection
@@ -14,9 +14,14 @@
 use Dashifen\WPHandler\Handlers\HandlerException;
 use Dashifen\ACFSharingSettings\ACFSharingSettings;
 
-(function () {
+if (file_exists($autoloader = dirname(ABSPATH) . '/deps/vendor/autoload.php'));
+elseif ($autoloader = file_exists(dirname(ABSPATH) . '/vendor/autoload.php'));
+elseif ($autoloader = file_exists(ABSPATH . 'vendor/autoload.php'));
+else $autoloader = 'vendor/autoload.php';
+require_once $autoloader;
+
+(function() {
     try {
-        ACFSharingSettings::requireAutoloader();
         $acfSharingSettings = new ACFSharingSettings();
         $acfSharingSettings->initialize();
     } catch (HandlerException $e) {
