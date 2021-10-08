7bb1140b0eda834644e9345bbb557632d2e611c1
David Dashifen Kees <dashifen@dashifen.com>
2021-10-08 06:33:30 -0400

    better autoloading

--- acf-sharing-settings.php
+++ acf-sharing-settings.php
@@ -1,24 +1,18 @@
 <?php
-
 /**
  * Plugin Name: ACF Sharing Settings
  * Description: An ACF field group for a settings page relating to how a site appears on Twitter and Facebook.
  * Author URI: mailto:dashifen@dashifen.com
  * Author: David Dashifen Kees
- * Version: 1.5.4
- *
- * @noinspection PhpStatementHasEmptyBodyInspection
- * @noinspection PhpIncludeInspection
+ * Version: 2.0.0
  */
 
 use Dashifen\WPHandler\Handlers\HandlerException;
 use Dashifen\ACFSharingSettings\ACFSharingSettings;
 
-if (file_exists($autoloader = dirname(ABSPATH) . '/deps/vendor/autoload.php'));
-elseif ($autoloader = file_exists(dirname(ABSPATH) . '/vendor/autoload.php'));
-elseif ($autoloader = file_exists(ABSPATH . 'vendor/autoload.php'));
-else $autoloader = 'vendor/autoload.php';
-require_once $autoloader;
+if (!class_exists('Dashifen\ACFSharingSettings\ACFSharingSettings')) {
+  require_once 'vendor/autoload.php';
+}
 
 (function() {
     try {
